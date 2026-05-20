0499a416ee7181a0ce0f950c8dbf03af3a48989b
Aditya Patil <adityabpatil489@gmail.com>
2026-05-20 20:24:28 +0530

    Fix remote configuration in push script

--- push_to_github.ps1
+++ push_to_github.ps1
@@ -3,14 +3,23 @@
 $REPO_NAME = "self-healing-devops-pipeline"
 $GITHUB_USERNAME = "patil612"
 
-Write-Host "Updating git remote URL to point to https://github.com/$GITHUB_USERNAME/$REPO_NAME.git"
-git remote set-url origin "https://github.com/$GITHUB_USERNAME/$REPO_NAME.git"
+# Check if origin already exists
+$remoteExists = git remote | Where-Object { $_ -eq "origin" }
+
+if ($remoteExists) {
+    Write-Host "Updating git remote URL to point to https://github.com/$GITHUB_USERNAME/$REPO_NAME.git"
+    git remote set-url origin "https://github.com/$GITHUB_USERNAME/$REPO_NAME.git"
+} else {
+    Write-Host "Adding git remote origin pointing to https://github.com/$GITHUB_USERNAME/$REPO_NAME.git"
+    git remote add origin "https://github.com/$GITHUB_USERNAME/$REPO_NAME.git"
+}
 
 Write-Host "Staging any unstaged changes..."
 git add .
 
+# Try to commit, ignore if there's nothing to commit
 Write-Host "Committing changes..."
-git commit -m "Configure self-healing devops pipeline repository"
+git commit -m "Configure self-healing devops pipeline repository" 2>$null
 
 Write-Host "Setting default branch to main..."
 git branch -M main
